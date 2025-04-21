d1a71a9bc80a8d1e95f923b03f666fd22a318e94
Khoa Nguyen <dangkhoaicao@gmail.com>
2025-04-21 13:28:25 +0700

    feat: Refactor PrimeVue component registration and add isVueComponent utility

--- apps/website/src/main.ts
+++ apps/website/src/main.ts
@@ -3,14 +3,16 @@ import { createApp } from 'vue'
 import App from './App.vue';
 import DirectusService from './services/directus.service';
 import PrimeVue from 'primevue/config';
-import InputText from 'primevue/inputtext';
-import InputNumber from 'primevue/inputnumber';
-import Select from 'primevue/select';
-import TextArea from 'primevue/textarea';
-import Button from 'primevue/button';
-import DatePicker from 'primevue/datepicker';
-import Divider  from 'primevue/divider';
-import Lara from '@primeuix/themes/lara'; // Thay thế Aura bằng LaraDark
+
+import * as PrimeVueComponents from 'primevue';
+// import InputText from 'primevue/inputtext';
+// import InputNumber from 'primevue/inputnumber';
+// import Select from 'primevue/select';
+// import TextArea from 'primevue/textarea';
+// import Button from 'primevue/button';
+// import DatePicker from 'primevue/datepicker';
+// import Divider  from 'primevue/divider';
+import Lara from '@primeuix/themes/lara';
 
 import ToastService from 'primevue/toastservice';
 
@@ -22,6 +24,7 @@ import DymikForm from 'dymik-core';
 import 'dymik-core/dist/dymik-core.css';
 
 import router from './router';
+import { isVueComponent } from './utils/isVueComponent';
 
 DirectusService.init(import.meta.env.VITE_DIRECTUS_URL, import.meta.env.VITE_DIRECTUS_STATIC_TOKEN);
 
@@ -39,14 +42,21 @@ app.use(ToastService);
 
 app.use(router);
 
-// Register PrimeVue components globally
-app.component('InputText', InputText);
-app.component('InputNumber', InputNumber);
-app.component('Select', Select);
-app.component('TextArea', TextArea);
-app.component('Button', Button);
-app.component('DatePicker', DatePicker);
-app.component('Divider', Divider);
+// // Register PrimeVue components globally
+// app.component('InputText', InputText);
+// app.component('InputNumber', InputNumber);
+// app.component('Select', Select);
+// app.component('TextArea', TextArea);
+// app.component('Button', Button);
+// app.component('DatePicker', DatePicker);
+// app.component('Divider', Divider);
+
+for (const key of Object.keys(PrimeVueComponents) as Array<keyof typeof PrimeVueComponents>) {
+    if (isVueComponent(PrimeVueComponents[key])) {
+        app.component(key, PrimeVueComponents[key]);
+    }
+}
 
 
 app.mount('#app')
+
